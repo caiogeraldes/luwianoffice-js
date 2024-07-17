a08bf0070697c666bab18b539cb63045d97afb6e
Caio Geraldes <caio.geraldes@usp.br>
2024-07-17 17:08:32 -0300

    base

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import luwian from "./converters/luwian.js";
+
+const allConverters = [
+    {
+        value: "luwian",
+        label: "Hieroglyphic Luwian",
+        converter: luwian
+    }
+]
+
+export {
+    luwian,
+    allConverters
+}
